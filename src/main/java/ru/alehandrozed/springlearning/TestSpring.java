527ac1840aa10728f9c446d95d83e10276c69398
”Zverev <alehandrozed@gmail.com>
2021-05-22 22:55:38 +0300

    part One create xml

---
+++ src/main/java/ru/alehandrozed/springlearning/TestSpring.java
@@ -0,0 +1,13 @@
+package ru.alehandrozed.springlearning;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
