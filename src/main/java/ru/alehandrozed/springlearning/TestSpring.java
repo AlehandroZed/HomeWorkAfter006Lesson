22ea1c46d7456e755b5f83703656aa3a0bb3d030
”Zverev <alehandrozed@gmail.com>
2021-05-23 22:44:26 +0300

    part Four Lesson06 (Dependency Injection (DI)part 2)

--- src/main/java/ru/alehandrozed/springlearning/TestSpring.java
+++ src/main/java/ru/alehandrozed/springlearning/TestSpring.java
@@ -18,6 +18,10 @@ public class TestSpring {
 
         player.playMusic();
 
+        System.out.println(player.getName());
+        System.out.println(player.getVolume());
+
+
         context.close();
     }
 }
