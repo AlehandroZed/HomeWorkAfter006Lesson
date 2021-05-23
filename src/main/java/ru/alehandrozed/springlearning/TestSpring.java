791ef2ec93613d8e4bbb2353dfe930b34aa6af85
”Zverev <alehandrozed@gmail.com>
2021-05-23 21:13:53 +0300

    part Two Lesson04 (Inversion of Control (IoC))

--- src/main/java/ru/alehandrozed/springlearning/TestSpring.java
+++ src/main/java/ru/alehandrozed/springlearning/TestSpring.java
@@ -6,8 +6,16 @@ public class TestSpring {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
+
+        //TestBean testBean = context.getBean("testBean", TestBean.class);
+        //System.out.println(testBean.getName());
+
+        Music music = context.getBean("musicBean", Music.class);
+
+        MusicPlayer player = new MusicPlayer(music);
+
+        player.playMusic();
+
         context.close();
     }
 }
