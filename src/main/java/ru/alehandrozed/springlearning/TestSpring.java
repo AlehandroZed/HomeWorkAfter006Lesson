b487c945f29abb6d27c250f3dd771b50cdc17cfc
”Zverev <alehandrozed@gmail.com>
2021-05-25 15:11:45 +0300

    Lesson08 part 1 (independent task)

--- src/main/java/ru/alehandrozed/springlearning/TestSpring.java
+++ src/main/java/ru/alehandrozed/springlearning/TestSpring.java
@@ -8,7 +8,8 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
 
-        Music music = context.getBean("musicBean1", RapMusic.class );
+        Music music1 = context.getBean("musicBean1", RapMusic.class );
+        Music music2 = context.getBean("musicBean1", RapMusic.class );
 
 //        MusicPlayer playerOne = context.getBean("musicPlayer", MusicPlayer.class);
 //        MusicPlayer playerTwo = context.getBean("musicPlayer", MusicPlayer.class);
@@ -23,7 +24,8 @@ public class TestSpring {
 //        System.out.println(playerOne.getVolume());
 //        System.out.println(playerTwo.getVolume());
 
-        System.out.println(music.getSong());
+        System.out.println(music1.getSong());
+        System.out.println(music2.getSong());
 
 
         context.close();
